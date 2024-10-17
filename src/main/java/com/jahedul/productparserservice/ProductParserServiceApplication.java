a1df94daeba7f2e64a74f92978f95602e956a790
jahedul <jahedulislam6060@gmail.com>
2024-10-17 21:21:58 +0600

    initial Pom setup

---
+++ src/main/java/com/jahedul/productparserservice/ProductParserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jahedul.productparserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductParserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductParserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jahedul/productparserservice/ProductParserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jahedul.productparserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductParserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
