b6f39df24da8cf2376f432c1d357a85e224679c7
jahedul <jahedulislam6060@gmail.com>
2024-10-19 02:54:42 +0600

    add custom product validator wrapper

---
+++ src/main/java/com/jahedul/productparserservice/util/ProductValidator.java
@@ -0,0 +1,30 @@
+package com.jahedul.productparserservice.util;
+
+import com.jahedul.productparserservice.entity.Product;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class ProductValidator {
+    private final Validator validator;
+
+    public void validate(Product product) {
+        Set<ConstraintViolation<Product>> violations = validator.validate(product);
+        if (!violations.isEmpty()) {
+            throw new IllegalArgumentException(buildValidationErrorMessage(product, violations));
+        }
+    }
+
+    private String buildValidationErrorMessage(Product product, Set<ConstraintViolation<Product>> violations) {
+        StringBuilder errorMessage = new StringBuilder("Validation error for SKU: " + product.getSku());
+        for (ConstraintViolation<Product> violation : violations) {
+            errorMessage.append(" - ").append(violation.getMessage());
+        }
+        return errorMessage.toString();
+    }
+}
