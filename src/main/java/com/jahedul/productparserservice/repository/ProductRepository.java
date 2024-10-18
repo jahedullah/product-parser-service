ea0c2c5eafb307453bb07fe605e1b5237789e4f5
jahedul <jahedulislam6060@gmail.com>
2024-10-18 13:02:34 +0600

    mark as repository

--- src/main/java/com/jahedul/productparserservice/repository/ProductRepository.java
+++ src/main/java/com/jahedul/productparserservice/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.jahedul.productparserservice.repository;
 
 import com.jahedul.productparserservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
 }
