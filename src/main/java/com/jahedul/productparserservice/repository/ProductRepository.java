64421394cbbf8e072b088fafff554b54bd72089a
jahedul <jahedulislam6060@gmail.com>
2024-10-19 01:57:14 +0600

    add method for finding all skus

--- src/main/java/com/jahedul/productparserservice/repository/ProductRepository.java
+++ src/main/java/com/jahedul/productparserservice/repository/ProductRepository.java
@@ -2,8 +2,13 @@ package com.jahedul.productparserservice.repository;
 
 import com.jahedul.productparserservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
+    @Query("SELECT p.sku FROM Product p")
+    List<String> findAllSkus();
 }
