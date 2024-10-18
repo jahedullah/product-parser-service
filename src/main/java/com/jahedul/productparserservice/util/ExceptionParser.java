48995e799c8fd71eb87ad473f888f692a2bece21
jahedul <jahedulislam6060@gmail.com>
2024-10-19 02:55:02 +0600

    add an exception msg parser

---
+++ src/main/java/com/jahedul/productparserservice/util/ExceptionParser.java
@@ -0,0 +1,16 @@
+package com.jahedul.productparserservice.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExceptionParser {
+    private static final Pattern SKU_PATTERN = Pattern.compile("([A-Z0-9-]+)(?=\\s*\\]$)");
+
+    public static String extractSkuFromExceptionMessage(String message) {
+        Matcher matcher = SKU_PATTERN.matcher(message);
+        if (matcher.find()) {
+            return matcher.group(1).trim();
+        }
+        return null;
+    }
+}
