02b307887d8d8f96d1e1cd4e695bac43160280ad
jahedul <jahedulislam6060@gmail.com>
2024-10-19 16:53:52 +0600

    add custom method in repository to find last summary

--- src/main/java/com/jahedul/productparserservice/repository/FileUploadSummaryRepository.java
+++ src/main/java/com/jahedul/productparserservice/repository/FileUploadSummaryRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FileUploadSummaryRepository extends JpaRepository<FileUploadSummary, Long> {
+    FileUploadSummary findFirstByOrderByUploadDateTimeDesc();
 }
