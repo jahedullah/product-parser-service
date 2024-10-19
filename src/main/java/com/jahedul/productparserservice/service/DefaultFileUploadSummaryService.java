531d0703beb7fa23388f6dc5b49d12396d0d4a8b
jahedul <jahedulislam6060@gmail.com>
2024-10-19 16:55:13 +0600

    impl of default services

--- src/main/java/com/jahedul/productparserservice/service/DefaultFileUploadSummaryService.java
+++ src/main/java/com/jahedul/productparserservice/service/DefaultFileUploadSummaryService.java
@@ -2,17 +2,34 @@ package com.jahedul.productparserservice.service;
 
 import com.jahedul.productparserservice.entity.FileUploadSummary;
 import com.jahedul.productparserservice.model.FileUploadSummaryResource;
+import com.jahedul.productparserservice.repository.FileUploadSummaryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.ZonedDateTime;
 
 @Service
+@RequiredArgsConstructor
 public class DefaultFileUploadSummaryService implements FileUploadSummaryService {
+    private final FileUploadSummaryRepository fileUploadSummaryRepository;
+
     @Override
+    @Transactional(readOnly = true)
     public FileUploadSummary getSummaryOfLastUploadedFile() {
-        return null;
+        return fileUploadSummaryRepository.findFirstByOrderByUploadDateTimeDesc();
     }
 
     @Override
-    public FileUploadSummary saveFileUploadSummary(FileUploadSummaryResource fileUploadSummaryResource) {
-        return null;
+    @Transactional
+    public void saveFileUploadSummary(FileUploadSummaryResource fileUploadSummaryResource) {
+        FileUploadSummary fileUploadSummary = new FileUploadSummary();
+        fileUploadSummary.setFileName(fileUploadSummaryResource.getFileName());
+        fileUploadSummary.setProductsUpdated(fileUploadSummaryResource.getProductsUpdated());
+        fileUploadSummary.setProductsAdded(fileUploadSummaryResource.getProductsAdded());
+        fileUploadSummary.setUploadDateTime(ZonedDateTime.now());
+
+        fileUploadSummaryRepository.save(fileUploadSummary);
+        fileUploadSummaryResource.setId(fileUploadSummary.getId());
     }
 }
--- src/main/java/com/jahedul/productparserservice/service/DefaultProductService.java
+++ src/main/java/com/jahedul/productparserservice/service/DefaultProductService.java
@@ -1,35 +1,138 @@
 package com.jahedul.productparserservice.service;
 
 import com.jahedul.productparserservice.entity.Product;
-import com.jahedul.productparserservice.model.ProductResource;
+import com.jahedul.productparserservice.exception.DuplicateSkuException;
+import com.jahedul.productparserservice.model.FileUploadSummaryResource;
+import com.jahedul.productparserservice.repository.ProductRepository;
+import com.jahedul.productparserservice.util.ExceptionParser;
+import com.jahedul.productparserservice.util.ProductExcelParser;
+import com.jahedul.productparserservice.util.ProductUploadResult;
+import com.jahedul.productparserservice.util.ProductValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
-public class DefaultProductService implements ProductService{
-    @Override
-    public Product getProductBySku(String sku) {
-        return null;
-    }
+@RequiredArgsConstructor
+public class DefaultProductService implements ProductService {
+    private final ProductRepository productRepository;
+    private final FileUploadSummaryService fileUploadSummaryService;
+    private final ProductExcelParser productExcelParser;
+    private final ProductValidator productValidator;
 
     @Override
-    public List<Product> getProducts() {
-        return null;
+    @Transactional(readOnly = true)
+    public Optional<Product> getProductBySku(String sku) {
+        return productRepository.findById(sku);
     }
+
 
     @Override
-    public Product saveProduct(Product product) {
-        return null;
+    @Transactional
+    public void uploadProductsFromFile(MultipartFile file) throws IOException {
+        List<Product> newProducts = productExcelParser.parseFileToProducts(file);
+
+        Set<String> existingSkus = new HashSet<>(productRepository.findAllSkus());
+
+        Set<String> newProductSkus = newProducts.stream()
+                .map(Product::getSku)
+                .collect(Collectors.toSet());
+
+        Set<String> duplicateSkus = newProductSkus.stream()
+                .filter(existingSkus::contains)
+                .collect(Collectors.toSet());
+
+        List<Product> existingProducts = productRepository.findAllById(duplicateSkus);
+        Map<String, Product> existingProductMap = existingProducts.stream()
+                .collect(Collectors.toMap(Product::getSku, p -> p));
+
+        ProductUploadResult productUploadResult = processProducts(newProducts, existingProductMap);
+
+        saveProducts(productUploadResult.getProductsToAdd(), productUploadResult.getProductsToUpdate());
+        saveFileUploadSummary(file.getOriginalFilename(), productUploadResult);
     }
+
+    private ProductUploadResult processProducts(List<Product> newProducts, Map<String, Product> existingProductMap) {
+        List<Product> productsToAdd = new ArrayList<>();
+        List<Product> productsToUpdate = new ArrayList<>();
+        int productsAdded = 0;
+        int productsUpdated = 0;
+
+        for (Product newProduct : newProducts) {
+            productValidator.validate(newProduct);
+
+            Product existingProduct = existingProductMap.get(newProduct.getSku());
+            if (existingProduct != null) {
+                if (productHasChanged(existingProduct, newProduct)) {
+                    updateExistingProduct(existingProduct, newProduct);
+                    productsToUpdate.add(existingProduct);
+                    productsUpdated++;
+                }
+            } else {
+                productsToAdd.add(newProduct);
+                productsAdded++;
+            }
+        }
+
+
+        return ProductUploadResult.builder()
+                .productsToAdd(productsToAdd)
+                .productsToUpdate(productsToUpdate)
+                .productsAdded(productsAdded)
+                .productsUpdated(productsUpdated)
+                .build();
+    }
+
+    private boolean productHasChanged(Product existingProduct, Product newProduct) {
+        return !existingProduct.getTitle().equals(newProduct.getTitle())
+                || !existingProduct.getPrice().equals(newProduct.getPrice())
+                || existingProduct.getQuantity() != newProduct.getQuantity();
+    }
+
+    private void updateExistingProduct(Product existingProduct, Product newProduct) {
+        existingProduct.setTitle(newProduct.getTitle());
+        existingProduct.setPrice(newProduct.getPrice());
+        existingProduct.setQuantity(newProduct.getQuantity());
+        existingProduct.setLastUpdated(ZonedDateTime.now());
+    }
+
+    private void saveProducts(List<Product> productsToAdd, List<Product> productsToUpdate) {
+        try {
+            Stream.of(productsToAdd, productsToUpdate)
+                    .filter(list -> !list.isEmpty())
+                    .forEach(productRepository::saveAll);
+
+        } catch (DuplicateKeyException ex) {
+            String sku = ExceptionParser.extractSkuFromExceptionMessage(ex.getMessage());
+            throw new DuplicateSkuException(sku);
+        }
+    }
+
+    private void saveFileUploadSummary(String fileName, ProductUploadResult result) {
+        FileUploadSummaryResource summaryResource = new FileUploadSummaryResource();
+        summaryResource.setFileName(fileName);
+        summaryResource.setUploadDateTime(ZonedDateTime.now());
+        summaryResource.setProductsAdded(result.getProductsAdded());
+        summaryResource.setProductsUpdated(result.getProductsUpdated());
+
+        fileUploadSummaryService.saveFileUploadSummary(summaryResource);
+    }
+
 
     @Override
-    public Product updateProduct(String sku, Product product) {
-        return null;
+    @Transactional(readOnly = true)
+    public Page<Product> getAllProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 
-    @Override
-    public void deleteProduct(String sku) {
-
-    }
 }
