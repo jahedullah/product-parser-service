e7b9f6b77fd4e6fd990393be9ee6ed195654857f
jahedul <jahedulislam6060@gmail.com>
2024-10-19 16:53:20 +0600

    add getter setter to summary resource

--- src/main/java/com/jahedul/productparserservice/model/FileUploadSummaryResource.java
+++ src/main/java/com/jahedul/productparserservice/model/FileUploadSummaryResource.java
@@ -1,12 +1,16 @@
 package com.jahedul.productparserservice.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.ZonedDateTime;
 
+@Getter
+@Setter
 public class FileUploadSummaryResource {
     private Long id;
     private String fileName;
     private ZonedDateTime uploadDateTime;
     private int productsAdded;
     private int productsUpdated;
-    private int productsDeleted;
 }
